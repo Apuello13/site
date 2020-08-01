b9348262afd42682d145eb394ae9924f9998b53c
andres-droid <andrespuello53@gmail.com>
2020-07-31 22:33:51 -0500

    matomo

--- index.php
+++ index.php
@@ -4,10 +4,26 @@ $saludo = "Hola mundo";
 <html>
     <head>
     <title>Home</title>
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://testphp.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/testphp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+
     </head>
 
     <body>
-        <h1><?php print($saludo);?></h1>
+        <h1>Bienvenido y gracias</h1>
     </body>
 
 </html>
